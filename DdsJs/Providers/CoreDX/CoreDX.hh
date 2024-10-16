7935d0bfe578ef14feeaa12365eab76d2a7e4155
Rolando Nieves <rolando.j.nieves@nasa.gov>
2024-10-16 11:43:23 -0400

    Refactor CoreDX `SequenceUtilities`
    Move the template definition out of `CoreDX.hh` and place
    it in its own header file. Also, add `SequenceUtilities` to an
    inner namespace `DdsJs::CoreDX`.

--- DdsJs/Providers/CoreDX/CoreDX.hh
+++ DdsJs/Providers/CoreDX/CoreDX.hh
@@ -9,7 +9,6 @@
 #define _DDSJS_DDSJS_PROVIDERS_COREDX_HH_
 
 #include <string>
-#include <vector>
 
 #include <dds/dds.hh>
 #include <dds/dds_builtin.hh>
@@ -18,92 +17,6 @@
 
 namespace DdsJs
 {
-
-template< typename SequenceType, typename ElementType >
-class SequenceUtilities
-{
-public:
-    using CppEntity = SequenceType;
-
-    static ElementType& At(CppEntity& theSeq, size_t idx)
-    { ElementType *result = *(seq_at(&theSeq, idx)); return *result; }
-
-    static void Clear(CppEntity& theSeq)
-    { seq_clear(&theSeq); }
-
-    static ElementType const& ConstAt(CppEntity const& theSeq, size_t idx)
-    { ElementType *result = *(seq_at(&theSeq, idx)); return *result; }
-
-    static size_t ConstSize(CppEntity const& theSeq)
-    { return seq_get_size(&theSeq); }
-
-    static void Init(CppEntity& theSeq)
-    { DDS_SEQ_INIT(theSeq); }
-
-    static void Resize(CppEntity& theSeq, size_t newSize)
-    { seq_set_size(&theSeq, newSize); }
-
-    static size_t Size(CppEntity& theSeq)
-    { return seq_get_size(&theSeq); }
-};
-
-
-template< typename ElementType >
-class SequenceUtilities< DDS::sequence< ElementType >, ElementType >
-{
-public:
-    using CppEntity = DDS::sequence< ElementType >;
-
-    static ElementType& At(DDS::sequence< ElementType >& theSeq, size_t idx)
-    { return theSeq[idx]; }
-
-    static void Clear(DDS::sequence< ElementType >& theSeq)
-    { theSeq.clear(); }
-
-    static ElementType const& ConstAt(DDS::sequence< ElementType > const& theSeq, size_t idx)
-    { return theSeq[idx]; }
-
-    static size_t ConstSize(DDS::sequence< ElementType > const& theSeq)
-    { return theSeq.size(); }
-
-    static void Init(DDS::sequence< ElementType >& theSeq)
-    { /* no-op for C++ */ return; }
-
-    static void Resize(DDS::sequence< ElementType >& theSeq, size_t newSize)
-    { theSeq.resize(newSize); }
-
-    static size_t Size(DDS::sequence< ElementType >& theSeq)
-    { return theSeq.size(); }
-};
-
-
-template< typename ElementType, unsigned Bounds >
-class SequenceUtilities< DDS::bounded_sequence< ElementType, Bounds >, ElementType >
-{
-public:
-    using CppEntity = DDS::bounded_sequence< ElementType, Bounds >;
-
-    static ElementType& At(CppEntity& theSeq, size_t idx)
-    { return theSeq[idx]; }
-
-    static void Clear(CppEntity& theSeq)
-    { theSeq.clear(); }
-
-    static ElementType const& ConstAt(CppEntity const& theSeq, size_t idx)
-    { return theSeq[idx]; }
-
-    static size_t ConstSize(CppEntity const& theSeq)
-    { return theSeq.size(); }
-
-    static void Init(CppEntity& theSeq)
-    { /* no-op for C++ */ return; }
-
-    static void Resize(CppEntity& theSeq, size_t newSize)
-    { theSeq.resize(newSize); }
-
-    static size_t Size(CppEntity& theSeq)
-    { return theSeq.size(); }
-};
 
 
 template< unsigned Bounds >
