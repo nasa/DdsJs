cc785cc3572580f6f6c4a16b7418fca9e535b6fd
Rolando Nieves <rolando.j.nieves@nasa.gov>
2024-10-16 13:23:36 -0400

    Refactor character string proxies
    Inject the provider-specific `StringUtilities` as a template
    argument.

--- DdsJs/Providers/CoreDX/Strings.hh
+++ DdsJs/Providers/CoreDX/Strings.hh
@@ -12,17 +12,16 @@
 
 #include <napi.h>
 
-#include <DdsJs/Providers/CoreDX/CoreDX.hh>
-
 
 namespace DdsJs {
 
+template< template < unsigned Bounds > typename StringUtilities >
 class UnboundedString
 {
 public:
     using NapiContainer = Napi::String;
     using MyStrUtil = StringUtilities< 0 >;
-    using CppEntity = MyStrUtil::CppEntity;
+    using CppEntity = typename MyStrUtil::CppEntity;
 
     static void FromCpp(Napi::Env env, CppEntity const& cppVal, NapiContainer& jsValOut);
 
@@ -34,8 +33,8 @@ public:
 };
 
 
-template< unsigned Bounds >
-class BoundedString/* : public UnboundedString */
+template< template < unsigned Bounds > typename StringUtilities, unsigned Bounds >
+class BoundedString
 {
 public:
     using NapiContainer = Napi::String;
@@ -55,19 +54,28 @@ public:
 };
 
 
-template< unsigned Bounds >
-void
-BoundedString< Bounds >::FromCpp(Napi::Env env, CppEntity const& cppVal, NapiContainer& jsValOut)
+template< template < unsigned Bounds > typename StringUtilities >
+typename UnboundedString< StringUtilities >::NapiContainer
+UnboundedString< StringUtilities >::NewInstance(Napi::Env env)
 {
-    // ConstContents() is expected to return a Napi::String::New(...) compatible
-    // value.
-    jsValOut = NewInstance(env, MyStrUtil::ConstContents(cppVal));
+    Napi::EscapableHandleScope scope(env);
+    NapiContainer result = NapiContainer::New(env, "");
+
+    return scope.Escape((napi_value)result).As< NapiContainer >();
 }
 
 
-template< unsigned Bounds >
+template< template < unsigned Bounds > typename StringUtilities >
 void
-BoundedString< Bounds >::FromJs(Napi::Env env, NapiContainer const& jsVal, CppEntity& cppValOut)
+UnboundedString< StringUtilities >::FromCpp(Napi::Env env, CppEntity const& cppVal, NapiContainer &jsValOut)
+{
+    jsValOut = NewInstance(env, cppVal);
+}
+
+
+template< template < unsigned Bounds > typename StringUtilities >
+void
+UnboundedString< StringUtilities >::FromJs(Napi::Env env, NapiContainer const& jsVal, CppEntity& cppValOut)
 {
     std::string text = jsVal.Utf8Value();
     // SetContents() is expected to always receive the source string as a C++
@@ -76,9 +84,43 @@ BoundedString< Bounds >::FromJs(Napi::Env env, NapiContainer const& jsVal, CppEn
 }
 
 
-template< unsigned Bounds >
-typename BoundedString< Bounds >::NapiContainer
-BoundedString< Bounds >::NewInstance(Napi::Env env)
+template< template < unsigned Bounds > typename StringUtilities >
+typename UnboundedString< StringUtilities >::NapiContainer
+UnboundedString< StringUtilities >::NewInstance(Napi::Env env, CppEntity const& cppInstance)
+{
+    Napi::EscapableHandleScope scope(env);
+    // ConstContents() is expected to return a Napi::String::New(...) compatible
+    // value.
+    NapiContainer result = NapiContainer::New(env, MyStrUtil::ConstContents(cppInstance));
+
+    return scope.Escape((napi_value)result).As< NapiContainer >();
+}
+
+
+template< template < unsigned Bounds > typename StringUtilities, unsigned Bounds >
+void
+BoundedString< StringUtilities, Bounds >::FromCpp(Napi::Env env, CppEntity const& cppVal, NapiContainer& jsValOut)
+{
+    // ConstContents() is expected to return a Napi::String::New(...) compatible
+    // value.
+    jsValOut = NewInstance(env, MyStrUtil::ConstContents(cppVal));
+}
+
+
+template< template < unsigned Bounds > typename StringUtilities, unsigned Bounds >
+void
+BoundedString< StringUtilities, Bounds >::FromJs(Napi::Env env, NapiContainer const& jsVal, CppEntity& cppValOut)
+{
+    std::string text = jsVal.Utf8Value();
+    // SetContents() is expected to always receive the source string as a C++
+    // std::string and convert as necessary.
+    MyStrUtil::SetContents(text, cppValOut);
+}
+
+
+template< template < unsigned Bounds > typename StringUtilities, unsigned Bounds >
+typename BoundedString< StringUtilities, Bounds >::NapiContainer
+BoundedString< StringUtilities, Bounds >::NewInstance(Napi::Env env)
 {
     Napi::EscapableHandleScope scope(env);
     NapiContainer result = NapiContainer::New(env, "");
@@ -90,9 +132,9 @@ BoundedString< Bounds >::NewInstance(Napi::Env env)
 // Even though CoreDX defines bounded strings as fixed arrays, when the
 // fixed strings are used as union members, the result of the union getter
 // is `const char*` instead of the fixed size typedef.
-template< unsigned Bounds >
-typename BoundedString< Bounds >::NapiContainer
-BoundedString< Bounds >::NewInstance(Napi::Env env, const char* cppInstance)
+template< template < unsigned Bounds > typename StringUtilities, unsigned Bounds >
+typename BoundedString< StringUtilities, Bounds >::NapiContainer
+BoundedString< StringUtilities, Bounds >::NewInstance(Napi::Env env, const char* cppInstance)
 {
     Napi::EscapableHandleScope scope(env);
 
